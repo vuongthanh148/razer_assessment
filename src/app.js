a007aa2ea205a2ad970ae6b2a8b46efeac2aca30
Stephen <vtthanh3@dto-tech.com>
2024-03-04 18:34:33 +0700

    feat: add global config

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 const express = require("express")
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const mongoSanitize = require('express-mongo-sanitize')
+const cors = require('cors')
+
 const app = express()
+
+
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(xss())
 
 app.get('/health', function (req, res) {
     res.send("Service is up!!!")
